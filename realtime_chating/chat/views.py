4ec822b9807a2a39cef636120bc028aacc7a1c60
devnarayank <94194256+devnarayank@users.noreply.github.com>
2024-09-22 01:18:18 +0530

    fixed issue of internal server error

--- realtime_chating/chat/views.py
+++ realtime_chating/chat/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from .models import Room, Message
+from django.views.generic import TemplateView
 from .serializers import RoomSerializer, MessageSerializer
 
 class RoomList(generics.ListCreateAPIView):
@@ -9,3 +10,6 @@ class RoomList(generics.ListCreateAPIView):
 class MessageList(generics.ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+class IndexView(TemplateView):
+    template_name = 'chat.html'
